e67ca95f5901a1944cf5c710e901d5e709bc8aec
ox-Frank-xo <frank.g.ascencio@gmail.com>
2026-05-10 02:35:54 +0200

    Delete ExerciseExtra.py

--- ExerciseExtra.py
+++
@@ -1,5 +0,0 @@
-cadena = "Hola a todos, Hola Mundo!!!"
-indice = cadena.find ("Hola")
-nueva_cadena = cadena.replace("Hola", "Adiós")
-print(f"Cadena original: {cadena}")
-print(f"Cadena modificada: {nueva_cadena}")
